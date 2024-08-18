f0405ca3637319b55af1ca07b6c2ceea854e1f7d
mahmudashif <mdamahmud2000@gmail.com>
2024-08-18 18:17:49 +0600

    feat: number guessing game interface

---
+++ script.js
@@ -0,0 +1,37 @@
+let p1 = document.querySelector(".p1")
+let p1btn = document.querySelector(".p1btn")
+let p2 = document.querySelector(".p2")
+let p2btn = document.querySelector(".p2btn")
+let showMsg = document.querySelector(".showMsg")
+let output = document.querySelector(".output")
+let errorShow = document.querySelector(".errorShow")
+let player = document.querySelector(".player")
+
+p1btn.addEventListener("click", function () {
+    if (p1.value == "") {
+        errorShow.classList.remove("d-none")
+        errorShow.innerHTML = "put a value"
+
+    }
+    else {
+        if (!(p1.value - 1 == 0 ? true : p1.value - 1)) {
+            errorShow.classList.remove("d-none")
+            errorShow.innerHTML = "Enter number only"
+        }
+        else {
+            if (p1.value > 10 || p1.value - 1 < 0) {
+                errorShow.classList.remove("d-none")
+                errorShow.innerHTML = "The number must be in 1 to 10"
+            }
+            else {
+                errorShow.classList.add("d-none")
+                player.innerHTML = "Player 2"
+                p2.classList.remove("d-none")
+                p2btn.classList.remove("d-none")
+                p1.classList.add("d-none")
+                p1btn.classList.add("d-none")
+
+            }
+        }
+    }
+})
